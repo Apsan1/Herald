f606f91b404ee30ef7b653370e6caf7dcf2918fc
Apsan1 <np03cs4a220109@heraldcollege.edu.np>
2023-02-27 20:59:27 +0545

    I.S.A workshop

---
+++ I.S.A/aps.py
@@ -0,0 +1,10 @@
+''' This module takes input from user of two numbers 
+and performs arithmetic operation of division and prints them. '''
+def main():
+    ''' This is the main function. '''
+    num1 = int(input('Enter the first number:'))
+    num2 = int(input('Enter the second number:'))
+    print(num1//num2)
+    print(num1/num2)
+if __name__ == "__main__":
+    main()
